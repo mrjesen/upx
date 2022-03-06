42fdc38c83d1c06fe17ec2b8f487612e2c6a5ab9
Jesen <jesen@protonmail.com>
2022-03-06 23:30:31 +0800

    1

--- src/conf.h
+++ src/conf.h
@@ -392,7 +392,7 @@ private:
 
 
 // magic constants for patching
-#define UPX_MAGIC_LE32          0x21585055      /* "UPX!" */
+#define UPX_MAGIC_LE32          0x2022331E      /* "UPX!" */
 #define UPX_MAGIC2_LE32         0xD5D0D8A1
 
 
